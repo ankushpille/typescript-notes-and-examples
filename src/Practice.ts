5eb2fb98523f0d3b9d23736193a81f7005f8a535
ankushpille <ankush.pille1@gmail.com>
2025-11-05 12:56:30 +0530

    declaring variables type

--- src/Practice.ts
+++ src/Practice.ts
@@ -1,3 +1,11 @@
 const title : string = 'typescript basics'
-console.log(title)
+const version :number = 22;
+const isComplete : boolean = true;
+const numArray : number[] = [101,102,103]
+const strArray : string[] = ['ankush','hello']
 
+console.log(title);
+console.log(version);
+console.log(isComplete);
+console.log(numArray);
+console.log(strArray);
